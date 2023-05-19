a46bc1f8de72eab83d7d3774f0ff36777aa5f4c8
Tysuiku <timothydong41@gmail.com>
2023-05-19 15:37:26 -0400

    rangeSumBST

--- leetcode_easys.js
+++ leetcode_easys.js
@@ -118,17 +118,73 @@ var twoSum = function (nums, target) {
 // Output: 4
 // Explanation: There are 4 positive integers and 0 negative integers. The maximum count among them is 4.
 
-var maximumCount = function (nums) {
-  let neg = 0;
-  let positive = 0;
+// var maximumCount = function (nums) {
+//   let neg = 0;
+//   let positive = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0) {
-      neg += 1;
-    } else if (nums[i] > 0) {
-      positive += 1;
-    }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] < 0) {
+//       neg += 1;
+//     } else if (nums[i] > 0) {
+//       positive += 1;
+//     }
+//   }
+
+//   return positive > neg ? positive : neg;
+// };
+
+// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+// Example 1:
+
+// Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+// Output: 32
+// Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+// Example 2:
+
+// Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+// Output: 23
+// Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 2 * 104].
+// 1 <= Node.val <= 105
+// 1 <= low <= high <= 105
+// All Node.val are unique.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  if (root === null) {
+    return 0;
   }
 
-  return positive > neg ? positive : neg;
+  let sum = 0;
+
+  if (root.val >= low && root.val <= high) {
+    sum += root.val;
+  }
+
+  if (root.val > low) {
+    sum += rangeSumBST(root.left, low, high);
+  }
+
+  if (root.val < high) {
+    sum += rangeSumBST(root.right, low, high);
+  }
+
+  return sum;
 };
