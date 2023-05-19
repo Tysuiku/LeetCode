b3df18fa25066cc9e4112e12104302dbac11364d
Tysuiku <timothydong41@gmail.com>
2023-05-19 19:57:52 -0400

    numOfStrings

--- leetcode_easys.js
+++ leetcode_easys.js
@@ -242,3 +242,51 @@ var maximumValue = function (strs) {
   }
   return maxVal;
 };
+
+// 1967. Number of Strings That Appear as Substrings in Word
+// Easy
+// 563
+// 30
+// company
+// Uber
+// Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+// A substring is a contiguous sequence of characters within a string.
+
+// Example 1:
+
+// Input: patterns = ["a","abc","bc","d"], word = "abc"
+// Output: 3
+// Explanation:
+// - "a" appears as a substring in "abc".
+// - "abc" appears as a substring in "abc".
+// - "bc" appears as a substring in "abc".
+// - "d" does not appear as a substring in "abc".
+// 3 of the strings in patterns appear as a substring in word.
+// Example 2:
+
+// Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+// Output: 2
+// Explanation:
+// - "a" appears as a substring in "aaaaabbbbb".
+// - "b" appears as a substring in "aaaaabbbbb".
+// - "c" does not appear as a substring in "aaaaabbbbb".
+// 2 of the strings in patterns appear as a substring in word.
+// Example 3:
+
+// Input: patterns = ["a","a","a"], word = "ab"
+// Output: 3
+// Explanation: Each of the patterns appears as a substring in word "ab".
+
+var numOfStrings = function (patterns, word) {
+  let count = 0;
+  for (let pattern of patterns) {
+    for (let i = 0; i <= word.length - pattern.length; i++) {
+      if (word.slice(i, i + pattern.length) === pattern) {
+        count++;
+        break;
+      }
+    }
+  }
+  return count;
+};
