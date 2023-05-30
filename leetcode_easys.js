7a08a5275688a86e40c6dbcc57ebbbf563c6b248
Tysuiku <timothydong41@gmail.com>
2023-05-30 12:38:49 -0400

    isPalindrone

--- leetcode_easys.js
+++ leetcode_easys.js
@@ -323,3 +323,37 @@ var anagramMappings = function (nums1, nums2) {
 
   return result;
 };
+
+// 9. Palindrome Number
+// Easy
+// 9.7K
+// 2.5K
+// company
+// Amazon
+// company
+// Facebook
+// company
+// Adobe
+// Given an integer x, return true if x is a
+// palindrome
+// , and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+var isPalindrome = function (x) {
+  return x.toString() === x.toString().split("").reverse().join("");
+};
