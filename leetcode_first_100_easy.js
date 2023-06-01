018fa8422cd0a296f04d569142fd1c28253ae006
Tysuiku <timothydong41@gmail.com>
2023-06-01 13:16:36 -0400

    merge 2 sorted list

--- leetcode_first_100_easy.js
+++ leetcode_first_100_easy.js
@@ -77,3 +77,29 @@ const isValid = (s) => {
 
   return stack.length === 0;
 };
+
+// 21 merge two sorted list
+const mergeTwoLists = (l1, l2) => {
+  let curr = new ListNode();
+  const dummy = curr;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+
+  if (l1) {
+    curr.next = l1;
+  }
+  if (l2) {
+    curr.next = l2;
+  }
+
+  return dummy.next;
+};
