a117fa26efe2cc810ea8b900a1e3e9faf6ae751a
Tysuiku <timothydong41@gmail.com>
2023-06-01 09:45:25 -0400

    question 3

--- leetcode_first_100_easy.js
+++ leetcode_first_100_easy.js
@@ -20,3 +20,20 @@ let twoSum = (nums, target) => {
 const isPalindrome = (x) => {
   return x.toString() === x.toString().split("").reverse().join("");
 };
+
+// 3 roman to integer
+
+const romanToInt = (s) => {
+  let map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let val = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < map[s[i + 1]]) {
+      val -= map[s[i]];
+    } else {
+      val += map[s[i]];
+    }
+  }
+
+  return val;
+};
