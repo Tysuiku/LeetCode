ea066039f6d13a5ed5d54d1f5cdbd2fa985aed96
Tysuiku <timothydong41@gmail.com>
2023-06-01 13:40:49 -0400

    removdupagain

--- leetcode_first_100_easy.js
+++ leetcode_first_100_easy.js
@@ -103,3 +103,16 @@ const mergeTwoLists = (l1, l2) => {
 
   return dummy.next;
 };
+
+// 26 remove duplicates from sorted array
+
+const removeDuplicates = (nums) => {
+  let k = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
