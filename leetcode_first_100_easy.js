c28d367e7753272643a7d6570861b8ede4f405c5
Tysuiku <timothydong41@gmail.com>
2023-06-01 17:15:34 -0400

    search insert

--- leetcode_first_100_easy.js
+++ leetcode_first_100_easy.js
@@ -145,3 +145,22 @@ const strStr = (haystack, needle) => {
 
   return -1;
 };
+
+// 35 search insert position
+
+const searchInsert = (nums, target) => {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return low;
+};
