b4c656ee423b26665f0844c711fe2a0a2e8ce6dc
Tysuiku <timothydong41@gmail.com>
2023-06-01 13:45:23 -0400

    remove ele again

--- leetcode_first_100_easy.js
+++ leetcode_first_100_easy.js
@@ -116,3 +116,17 @@ const removeDuplicates = (nums) => {
   }
   return k;
 };
+
+// 27 remove element
+
+const removeElement = (nums, val) => {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
