6099d003934ff5c186ab0b6bcd07c4e687b11495
Tysuiku <timothydong41@gmail.com>
2023-06-02 10:51:57 -0400

    addBinary

--- leetcode_first_100_easy.js
+++ leetcode_first_100_easy.js
@@ -185,3 +185,26 @@ const plusOne = (digits) => {
   digits.unshift(1);
   return digits;
 };
+
+// 67 add binary
+
+// const addBinary = (a, b) => {
+//   let sum = parseInt(a, 2) + parseInt(b, 2);
+//   return sum.toString(2);
+// };
+
+const addBinary = (a, b) => {
+  let result = "";
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+  while (i >= 0 || j >= 0) {
+    let sum = carry;
+    if (i >= 0) sum += a[i--] - "0";
+    if (j >= 0) sum += b[j--] - "0";
+    carry = Math.floor(sum / 2);
+    result += sum % 2;
+  }
+  if (carry) result += carry;
+  return result.split("").reverse().join("");
+};
