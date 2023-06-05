b9bbd8e27e478a2d3782716dd8d0c78399da144c
Tysuiku <timothydong41@gmail.com>
2023-06-05 19:30:57 -0400

    q3

--- blind75.js
+++ blind75.js
@@ -27,3 +27,16 @@ const maxProfit = (prices) => {
 
   return maxProfit;
 };
+
+//217
+const containsDuplicate = (nums) => {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] !== undefined) {
+      return true;
+    } else {
+      map[nums[i]] = 1;
+    }
+  }
+  return false;
+};
