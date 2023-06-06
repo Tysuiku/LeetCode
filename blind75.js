666386ef771a3ca439f3ee60e8143f527a8e36d0
Tysuiku <timothydong41@gmail.com>
2023-06-06 04:22:02 -0400

    q4

--- blind75.js
+++ blind75.js
@@ -40,3 +40,22 @@ const containsDuplicate = (nums) => {
   }
   return false;
 };
+
+//238
+const productExceptSelf = (nums) => {
+  let length = nums.length;
+
+  let answer = [];
+  answer[0] = 1;
+
+  for (let i = 1; i < length; i++) {
+    answer[i] = nums[i - 1] * answer[i - 1];
+  }
+  let R = 1;
+  for (let i = length - 1; i >= 0; i--) {
+    answer[i] = answer[i] * R;
+    R *= nums[i];
+  }
+
+  return answer;
+};
