7e5ee47cedc3b4a669cc0dab85eb01ce9a2adbb5
Tysuiku <timothydong41@gmail.com>
2023-06-12 17:08:18 -0400

    daily

--- blind75.py
+++ blind75.py
@@ -65,3 +65,27 @@ class Solution(object):
             max_sum = max(max_sum, cur_sum)
 
         return max_sum
+
+
+# 152
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        max_prod = min_prod = result = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                max_prod, min_prod = min_prod, max_prod
+
+            max_prod = max(nums[i], max_prod * nums[i])
+            min_prod = min(nums[i], min_prod * nums[i])
+
+            result = max(result, max_prod)
+
+        return result
